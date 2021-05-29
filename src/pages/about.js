e22dd93461e9ba7a97198afee9800a0bf5c73c25
Karthikeya <karthikeya.s@melento.com>
2021-05-29 22:01:37 +0530

    gatsby-plugin-image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,17 @@
 // Step 1: Import your component
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from './layout'
+import {StaticImage} from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        <StaticImage
+        src="../images/Landscape-Color.jpg"
+        alt="Beautiful Landscape color"
+        />
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from './layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -9,6 +9,11 @@ const IndexPage = () => {
     <Layout pageTitle="Home">
       <p>Welcome to my Gatsby site!</p>
       <h6>I created this using Gatsby.</h6>
+      <StaticImage 
+      src="https://st.depositphotos.com/1428083/2946/i/600/depositphotos_29460297-stock-photo-bird-cage.jpg"
+      alt="Free bird"
+      />
+      
     </Layout>
   )
 }
